3d74ca57d31a62dddb5d7a8c567502ea0c10c3ed
Dhrubajit <dhrubajit1992@gmail.com>
2020-08-27 17:03:30 +0800

    fix storybook for snowpack

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,36 @@
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  webpackFinal: async (config) => {
+    // config.module.rules.push({
+    //   test: /\.css$/i,
+    //   use: ["style-loader", "css-loader"],
+    // });
+
+    config.module.rules.push({
+      test: /\.js$/,
+      exclude: /(node_modules)/,
+      use: {
+        loader: "babel-loader",
+        options: {
+          plugins: [
+            [
+              "@babel/plugin-proposal-decorators",
+              {
+                legacy: true,
+              },
+            ],
+            [
+              "@babel/plugin-proposal-class-properties",
+              {
+                loose: true,
+              },
+            ],
+          ],
+        },
+      },
+    });
+
+    return config;
+  },
 };
